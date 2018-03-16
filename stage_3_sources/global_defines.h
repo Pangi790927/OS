b8372ca71f41029d49d858b625513ba660a7986f
Pangi <andrpangr@gmail.com>
2018-03-16 07:21:55 +0200

    forgot to add new KERNEL_ON_HDD location

--- stage_3_sources/global_defines.h
+++ stage_3_sources/global_defines.h
@@ -3,7 +3,7 @@
 
 #define CODE_SEL		0x08
 #define DATA_SEL		0x10
-#define KERNEL_ON_HDD	0x8000
+#define KERNEL_ON_HDD	0x10000
 #define STACK_START		0x0efffff
 #define KERNEL_IN_RAM	0x1000000
 #define HEAP_START		0x2000000
